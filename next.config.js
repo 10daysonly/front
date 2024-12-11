4d7ed5df2891abe157c202c025ef3aac41e6b37a
theblessedcity <theblessedcity@hanmail.net>
2024-12-11 17:31:25 +0900

    fix: import svg file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 export default {
   reactStrictMode: true,
   // 기타 설정
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"], // SVG 파일을 컴포넌트로 가져오기
+    });
+    return config;
+  },
 };
