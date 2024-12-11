c3592046076c1603b519683b36184b82ba65cfbd
theblessedcity <theblessedcity@hanmail.net>
2024-12-11 14:05:32 +0900

    fix: big about state for stories - Input, Textarea

--- src/components/DataInfo/DataInfo.tsx
+++ src/components/DataInfo/DataInfo.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import "./DataInfo.scss";
 
 interface Props {
--- src/components/Textarea/Textarea.stories.tsx
+++ src/components/Textarea/Textarea.stories.tsx
@@ -20,12 +20,15 @@ type Story = StoryObj<typeof meta>;
 export const Example: Story = {
   args: {},
   decorators: [
-    (Story) => {
+    (Story, context) => {
       const [value, setValue] = React.useState("");
-      const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+      const handleChange: React.ChangeEventHandler<HTMLTextAreaElement> = (e) => {
         setValue(e.target.value);
       };
-      return <Story value={value} onChange={handleChange} />;
+
+      const modifedArgs = { ...context.args, value, onChange: handleChange };
+
+      return <Story {...context} args={modifedArgs} />;
     },
   ],
 };
