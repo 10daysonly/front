737c1c6dddc60b5ee78bd6cb194d2eba6dcb5e03
BoJeong Ko <118041575+uglysweetpotatos@users.noreply.github.com>
2024-12-15 00:57:09 +0900

    마니또 뽑기 구현

--- src/app/gatherings/[gatheringId]/participants/_roomInfo/randomDraw/randomDraw.tsx
+++ src/app/gatherings/[gatheringId]/participants/_roomInfo/randomDraw/randomDraw.tsx
@@ -6,21 +6,16 @@ import { useWebSocket } from "../../_webSockect/webSocket";
 import { useParams, useSearchParams } from "next/navigation";
 
 export default function RandomDraw() {
-  const [value, setValue] = useState(1);
+  const [whichGame, setWhichGame] = useState("secret_santa");
   const { inviteCard } = useAppSelector((state) => state.inviteCardSlice);
   const { gatheringId } = useParams();
   const searchParams = useSearchParams();
   const token = searchParams.get("token"); // 쿼리스트링에서 'token'의 값을 가져옴
 
-  const onChange = (e: any) => {
-    console.log("radio checked", e.target.value);
-    setValue(e.target.value);
-  };
-
   const onclick = async () => {
     const response = await axios.post(
       `/api/postGames/${gatheringId}`,
-      JSON.stringify({ type: "random_pick", token: token }),
+      JSON.stringify({ type: whichGame, token: token }),
       {
         headers: {
           "Content-Type": "application/json", // 헤더 설정
@@ -37,22 +32,32 @@ export default function RandomDraw() {
     <div>
       <p>랜덤 뽑기</p>
       <div>
-        <div>연결 상태: {isConnected ? "연결됨" : "연결 안됨"}</div>
+        {/* <div>연결 상태: {isConnected ? "연결됨" : "연결 안됨"}</div> */}
         <div>
-          {messages.map((msg, index) => (
-            <div key={index}>{JSON.stringify(msg)}</div>
-          ))}
+          <div>
+            {messages ? (
+              <div>
+                {messages?.[messages.length - 1]?.data?.results?.find(
+                  (item: any) => item.receiver === "길똥이"
+                )?.giver || "정보 없음"}
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
         </div>
       </div>
       <Radio.Group
-        onChange={onChange}
-        value={value}
+        onChange={(e) => {
+          setWhichGame(e.target.value);
+        }}
+        value={whichGame}
         optionType="button"
         buttonStyle="solid"
         style={{ display: "flex", flexDirection: "column", gap: "8px" }} // 세로 정렬 및 간격 추가
       >
-        <Radio.Button value="option1">마니또 뽑기(본인 제외 랜덤)</Radio.Button>
-        <Radio.Button value="option2">랜덤 뽑기(완전 무작위)</Radio.Button>
+        <Radio.Button value="secret_santa">마니또 뽑기(본인 제외 랜덤)</Radio.Button>
+        <Radio.Button value="random_pick">랜덤 뽑기(완전 무작위)</Radio.Button>
       </Radio.Group>
       <Button onClick={onclick}>시작</Button>
     </div>
