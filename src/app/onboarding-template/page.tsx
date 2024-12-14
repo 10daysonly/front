4a101f4d63590fa05df24363ceed2b1559e5f1e0
theblessedcity <theblessedcity@hanmail.net>
2024-12-15 03:44:06 +0900

    fix: revert src/app/onboarding-template

---
+++ src/app/onboarding-template/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+import NextImage from "next/image";
+
+import { useRouter } from "next/navigation";
+
+import "./Onboarding.scss";
+import "./onboarding.module.css"; // custom css
+
+import Layout from "@/components/Layout";
+import Main from "@/components/Main";
+import Button from "@/components/Button";
+import ButtonBox from "@/components/ButtonBox";
+
+import logo from "./imgs/logo-onboarding.svg";
+
+export default function Home() {
+  const router = useRouter();
+  return (
+    <Layout page="onboarding">
+      <Main>
+        <div className={`ui-fullpage`}>
+          <div className={`onboarding`}>
+            <div className={`onboarding-box`}>
+              <div className={`onboarding-logo`}>
+                <NextImage src={logo.src} width={logo.width} height={logo.height} alt={``} />
+              </div>
+              <p className={`onboarding-title`}>
+                Make A <br />
+                Ringley Letter !
+              </p>
+              <p className={`onboarding-subtitle`}>링글리를 통해 초대해 보아요!</p>
+            </div>
+          </div>
+          <div className={`fixed-bottom`}>
+            <ButtonBox>
+              <Button
+                block={true}
+                size="large"
+                color={`primary`}
+                onClick={() => {
+                  router.push("/invite/upsert");
+                }}
+              >
+                시작하기
+              </Button>
+            </ButtonBox>
+          </div>
+        </div>
+      </Main>
+    </Layout>
+  );
+}
